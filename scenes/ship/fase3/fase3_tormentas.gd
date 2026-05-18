9998b7734701a4c34604282a563f3df017e9ecd3
Lucas Costa Izidoro <lucas.izidorolci@gmail.com>
2026-05-18 00:05:20 -0300

    Mecanicas,sprites,ondas,HUD implementados

--- scenes/ship/fase3/fase3_tormentas.gd
+++ scenes/ship/fase3/fase3_tormentas.gd
@@ -1,23 +1,39 @@
 extends Node2D
 
 @onready var carvela = $Carvela
-@onready var barra_vida = $HUD/BarraVida
+@onready var barra_vida: TextureProgressBar = $HUD/BarraVida
+@onready var spawn_timer: Timer = $swapnTimer
+
+var onda_cena = preload("res://scenes/ship/fase3/onda.tscn")
+var faixas: Array[float] = [60, 120.0, 180.0]
 
 
 func _ready() -> void:
-	# Conecta o sinal da caravela à barra de vida
 	carvela.health_changed.connect(_on_health_changed)
-	# Inicia a barra com vida cheia
 	barra_vida.max_value = carvela.max_health
 	barra_vida.value = carvela.health
+	spawn_timer.timeout.connect(_spawnar_onda)
+
+
+func _spawnar_onda() -> void:
+	print("Spawnando onda!")
+	var onda = onda_cena.instantiate()
+	var faixa = faixas[randi() % faixas.size()]
+	onda.position = Vector2(800, faixa)
+	var tipos = ["pequena", "media", "grande"]
+	onda.tipo = tipos[randi() % tipos.size()]
+	add_child(onda)
 
 
 func _on_health_changed(current: float, maximum: float) -> void:
 	barra_vida.value = current
-	# Muda a cor conforme a vida
-	if current / maximum <= 0.3:
-		barra_vida.modulate = Color(1, 0, 0)  # vermelho
-	elif current / maximum <= 0.6:
-		barra_vida.modulate = Color(1, 0.6, 0)  # laranja
+	var ratio := current / maximum
+
+	if ratio > 0.75:
+		barra_vida.modulate = Color(1, 1, 1)
+	elif ratio > 0.5:
+		barra_vida.modulate = Color(1, 1, 0)
+	elif ratio > 0.25:
+		barra_vida.modulate = Color(1, 0.5, 0)
 	else:
-		barra_vida.modulate = Color(1, 1, 1)  # normal
+		barra_vida.modulate = Color(1, 0, 0)
